ae7fb94aef71ffa0b52dfa07df717f2948b3c280
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-08-15 20:47:39 -0700

    added inquirer modules, and prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,46 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this applicationconst 
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description?',
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage information',
+        message: 'Usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'test instructions',
+        message: 'Test instructions?',
+    },
+])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
